0ea5f25daa001eba8d7735f15e2755a444c2d349
TheRealVDevelopers <runfast060425@gmail.com>
2026-02-25 12:03:16 +0530

    ok

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: true,
     port: 8080,
     hmr: {
       overlay: false,
     },
+  },
+  build: {
+    target: ["es2015", "safari14", "ios14"],
+    cssTarget: ["safari14", "ios14"],
   },
   plugins: [react()].filter(Boolean),
   resolve: {
